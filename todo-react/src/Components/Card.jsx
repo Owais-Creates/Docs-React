c72131524e1a7d435294b6e9656341cf8b5bfd5f
Owais101-afk <owaisali753@gmail.com>
2024-01-01 13:55:28 +0530

    Second commit

--- todo-react/src/Components/Card.jsx
+++ todo-react/src/Components/Card.jsx
@@ -1,15 +1,32 @@
 import React from 'react'
 import { FaRegFileAlt } from "react-icons/fa";
+import { MdOutlineFileDownload } from "react-icons/md";
+import { IoIosCloseCircleOutline } from "react-icons/io";
+import { motion } from "framer-motion"
 
 
-function Card() {
+function Card({ data, reference }) {
     return (
         <>
-            <div className=' relative w-60 h-72 rounded-[50px] bg-zinc-900/90 text-white px-6 py-10 overflow-hidden '>
-                <FaRegFileAlt/>
-                <p className='text-sm font-semibold mt-3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex, corporis.</p>
-                <div className='bg-sky-200 w-full h-12 absolute bottom-0 left-0 '></div>
-            </div>
+            <motion.div
+                dragConstraints={reference} drag whileDrag={{ scale: 1.2 }} dragTransition={{ bounceStiffness: 600, bounceDamping: 10 }}
+                className='relative w-60 h-72 rounded-[45px] bg-zinc-900/90 text-white px-6 py-10 overflow-hidden'>
+                <FaRegFileAlt />
+                <p className='text-sm font-semibold mt-3'>{data.desc}.</p>
+                <div className=' footer w-full absolute bottom-0 left-0  '>
+                    <div className='flex items-center justify-between px-8 py-3 mb-3 text-white'>
+                        <h4>{data.fileSize}</h4>
+                        {data.close ? <IoIosCloseCircleOutline /> : <MdOutlineFileDownload />}
+                    </div>
+
+                    {data.tag.isOpen && (
+                        <div className='data py-6 w-full h-8 bg-green-600 flex items-center justify-center' >
+                            <p className='font-semibold text-md'>Download</p>
+                        </div>
+                    )}
+
+                </div>
+            </motion.div>
 
         </>
     )
--- todo-react/src/Components/Foreground.jsx
+++ todo-react/src/Components/Foreground.jsx
@@ -1,11 +1,41 @@
-import React from 'react'
+import React, { useRef } from 'react'
 import Card from './Card'
 
 function Foreground() {
+
+    const ref = useRef(null);
+
+    const data = [
+        {
+            desc: "Hello World",
+            fileSize: "0.9mb",
+            close: "false",
+            tag: { isOpen: "false", tagTitle: "Download Now", tagColor: "green" }
+        },
+        {
+            desc: "Hello World",
+            fileSize: "0.9mb",
+            close: "false",
+            tag: { isOpen: "false", tagTitle: "Download Now", tagColor: "green" }
+        },
+        {
+            desc: "Hello World",
+            fileSize: "0.9mb",
+            close: "false",
+            tag: { isOpen: "false", tagTitle: "Download Now", tagColor: "green" }
+        }
+    ]
+
     return (
         <>
-            <div className='w-full h-screen fixed z-[3]'></div>
-            <Card />
+            <div ref={ref} className='w-full h-screen fixed top-0 left-0 z-[3] p-5 flex gap-10 flex-wrap'>
+
+                {data.map((items, index) => (
+                    <Card data={items} reference={ref} />
+                ))}
+
+            </div>
+
         </>
     )
 }
